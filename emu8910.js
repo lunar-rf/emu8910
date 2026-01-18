846f99fcfea064c7e9199dbc15e80a583bc0f131
dmlunar <md.node.0@gmail.com>
2026-01-18 15:12:54 +0200

    Update emu8910.js

--- emu8910.js
+++ emu8910.js
@@ -21,7 +21,7 @@
  * | https://lunar.sh                      |
  * |                                       |
  * | Research Laboratories                 |
- * | Donate XMR @ 'lunar.sh' (OpenAlias)   |
+ * | OpenAlias (BTC, XMR): lunar.sh        |
  * | Copyright (C) 2022-2024               |
  * +---------------------------------------+
  *
